a2f573ed8bfb6244994b7282b96ffe488f2da212
Your Name <you@example.com>
2026-08-29 21:51:25 +0200

    temp sleeping bag model

--- src/main/java/com/CuteNekoDragon/Core/common/data/blocks/SVOBlocks.java
+++ src/main/java/com/CuteNekoDragon/Core/common/data/blocks/SVOBlocks.java
@@ -1,9 +1,16 @@
 package com.CuteNekoDragon.Core.common.data.blocks;
 
+import com.CuteNekoDragon.Core.common.block.SleepingBagBlock;
+import com.tterrag.registrate.providers.loot.RegistrateBlockLootTables;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackBlock;
 
 import com.CuteNekoDragon.Core.SVOCore;
 import com.tterrag.registrate.util.entry.BlockEntry;
+
+import static com.CuteNekoDragon.Core.SVOCore.REGISTRATE;
 
 public final class SVOBlocks {
 
@@ -13,5 +20,19 @@ public final class SVOBlocks {
             .block("iridium_backpack", p -> new BackpackBlock())
             .blockstate((ctx, prov) -> prov.simpleBlock(ctx.getEntry(),
                     prov.models().getExistingFile(SVOCore.id("block/iridium_backpack"))))
+            .register();
+
+    public static BlockEntry<SleepingBagBlock> WHITE_SLEEPING_BAG = REGISTRATE
+            .block("sleeping_bag", SleepingBagBlock::new)
+            .initialProperties(() -> Blocks.WHITE_WOOL)
+            .properties(p -> p.noOcclusion().strength(0.1F).sound(SoundType.WOOL))
+            .blockstate((ctx, prv) -> prv.horizontalBlock(
+                    ctx.getEntry(),
+                    prv.models().getExistingFile(SVOCore.id("block/sleeping_bag"))
+            ))
+            .loot(RegistrateBlockLootTables::dropSelf)
+            .item()
+            .model((ctx, prov) -> prov.withExistingParent(ctx.getName(), SVOCore.id("block/sleeping_bag")))
+            .build()
             .register();
 }
