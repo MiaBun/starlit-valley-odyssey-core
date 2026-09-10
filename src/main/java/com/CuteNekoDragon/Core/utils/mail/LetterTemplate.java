11a9c84e47b1c7cf86c5c032935c987d4e21768c
Your Name <you@example.com>
2026-09-10 23:55:49 +0200

    letter template

--- src/main/java/com/CuteNekoDragon/Core/utils/mail/LetterTemplate.java
+++ src/main/java/com/CuteNekoDragon/Core/utils/mail/LetterTemplate.java
@@ -1,4 +1,26 @@
 package com.CuteNekoDragon.Core.utils.mail;
 
+import lombok.Getter;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+
 public class LetterTemplate {
+
+    @Getter
+    private final ResourceLocation id;
+    @Getter
+    private final ResourceLocation defaultNpcId;
+    @Getter
+    private final String title;
+    @Getter
+    private final List<String> bodyLines;
+
+    public LetterTemplate(ResourceLocation id, ResourceLocation defaultNpcId, String title, List<String> bodyLines) {
+        this.id = id;
+        this.defaultNpcId = defaultNpcId;
+        this.title = title;
+        this.bodyLines = bodyLines;
+    }
+
 }
