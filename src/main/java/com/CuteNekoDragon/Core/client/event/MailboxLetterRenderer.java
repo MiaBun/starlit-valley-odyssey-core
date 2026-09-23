0518fe2be127f05cfa72d3cfbf67c18dac33f7ab
Your Name <you@example.com>
2026-09-24 01:31:23 +0200

    first bits of mailbox letter renderer, rip my dad...

---
+++ src/main/java/com/CuteNekoDragon/Core/client/event/MailboxLetterRenderer.java
@@ -0,0 +1,11 @@
+package com.CuteNekoDragon.Core.client.event;
+
+import com.CuteNekoDragon.Core.SVOCore;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SVOCore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class MailboxLetterRenderer {
+
+
+}
