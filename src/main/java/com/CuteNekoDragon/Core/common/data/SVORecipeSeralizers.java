4ac28e51ca008fb36758ecfd32fec2dce50bcfbc
Your Name <you@example.com>
2026-09-25 22:54:32 +0200

    sacktotoolbelt recipe

--- src/main/java/com/CuteNekoDragon/Core/common/data/SVORecipeSeralizers.java
+++ src/main/java/com/CuteNekoDragon/Core/common/data/SVORecipeSeralizers.java
@@ -2,6 +2,7 @@ package com.CuteNekoDragon.Core.common.data;
 
 import com.CuteNekoDragon.Core.common.recipe.serializers.SackToBackpackRecipe;
 import com.CuteNekoDragon.Core.common.recipe.serializers.SackToLunchboxRecipe;
+import com.CuteNekoDragon.Core.common.recipe.serializers.SackToToolbeltRecipe;
 import com.CuteNekoDragon.Core.common.recipe.serializers.SackUpgradeRecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,4 +19,6 @@ public class SVORecipeSeralizers {
     public static final RegistryObject<SackUpgradeRecipeSerializer<SackToBackpackRecipe>> SACK_TO_BACKPACK = RECIPE_SERIALIZERS.register("sack_to_backpack", () -> new SackUpgradeRecipeSerializer<>(SackToBackpackRecipe::new));
 
     public static final RegistryObject<SackUpgradeRecipeSerializer<SackToLunchboxRecipe>> SACK_TO_LUNCHBOX = RECIPE_SERIALIZERS.register("sack_to_lunchbox", () -> new SackUpgradeRecipeSerializer<>(SackToLunchboxRecipe::new));
+
+    public static final RegistryObject<SackUpgradeRecipeSerializer<SackToToolbeltRecipe>> SACK_TO_TOOLBELT = RECIPE_SERIALIZERS.register("sack_to_toolbelt", () -> new SackUpgradeRecipeSerializer<>(SackToToolbeltRecipe::new));
 }
---
+++ src/main/java/com/CuteNekoDragon/Core/common/recipe/serializers/SackToToolbeltRecipe.java
@@ -0,0 +1,29 @@
+package com.CuteNekoDragon.Core.common.recipe.serializers;
+
+import com.CuteNekoDragon.Core.common.data.SVORecipeSeralizers;
+import com.CuteNekoDragon.Core.common.item.ToolbeltItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.ShapedRecipe;
+
+import java.util.List;
+
+public class SackToToolbeltRecipe  extends AbstractSackUpgradeRecipe{
+
+    public SackToToolbeltRecipe(ShapedRecipe compose) {
+        super(compose);
+    }
+
+    @Override
+    protected void populateResult(ItemStack result, List<ItemStack> sackContents, List<ItemStack> leftover) {
+        ToolbeltItem item = (ToolbeltItem) result.getItem();
+        packMatching(result, sackContents, leftover, ItemStack::isDamageableItem,
+                item.getBaseStorageSize(), ToolbeltItem.TAG_StorageSize);
+        result.getOrCreateTag().putInt(ToolbeltItem.TAG_Slot, 1);
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return SVORecipeSeralizers.SACK_TO_TOOLBELT.get();
+    }
+}
